d823f52dca3c3b29e07e264d4c54201bd1af8a23
mohammad-akil <mohammadakilshakirhusain@gmail.com>
2024-04-05 15:24:03 +0530

    add test data

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
-const cors = require("cors");
 require("dotenv").config();
 
 const tasksRouter = require("./Router/tasks");
 
 const connectToDB = require("./connectdb");
+const testData = require("./data/test.json");
 
 const PORT = process.env.PORT || 3000;
 
@@ -17,19 +17,15 @@ app.use(
   })
 );
 
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST", "DELETE", "PATCH"],
-  })
-);
-
 //connection to mongoDb
 connectToDB();
 
 //routers
 
 app.use("/api/tasks", tasksRouter);
+app.use("/api/test", (req, res) => {
+  res.send({ testData });
+});
 
 app.listen(PORT, () => {
   console.log(`App is running on port ${PORT}`);
